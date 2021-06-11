31520d3104ef2a91aabfe7176939fa0881942234
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2021-06-11 20:21:59 +0600

    Update tests.py

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import BookList
 
-# Create your tests here.
+class Booklist(TestCase):
+    def setUp(self):
+        BookList.objects.create(name="lion", sound="roar")
+        BookList.objects.create(name="cat", sound="meow")
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        book1 = BookList.objects.get(name="hello_world")
+        book2 = BookList.objects.get(name="welcome to django world")
+        self.assertEqual(book1.write(), 'The lion says "travis"')
+        self.assertEqual(book2.write(), 'The cat says "shakespeer"')
